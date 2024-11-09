9bfeb003fbeea70804cd74ceded0245e1765a69b
KumarShresth7 <kumarshresth2004@gmail.com>
2024-11-09 18:50:07 +0530

    integrated socketio

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ const connectDB = require('./config/db')
 const authRoutes = require('./routes/authRoutes')
 const streamRoutes = require('./routes/StreamRoutes')
 const userRoutes = require('./routes/userRoutes')
+const http = require('http')
+const {Server} = require('socket.io')
 dotenv.config()
 
 const corsOptions = {
@@ -17,6 +19,29 @@ const corsOptions = {
     optionsSuccessStatus: 200,
 }
 
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: { origin: "*" },
+  });
+  
+  io.on("connection", (socket) => {
+    console.log("A user connected");
+  
+    socket.on("joinRoom", (room) => {
+      socket.join(room);
+      console.log(`User joined room: ${room}`);
+    });
+  
+    socket.on("sendMessage", (data) => {
+      io.to(data.room).emit("receiveMessage", data);
+      console.log(`Message sent to room ${data.room}: ${data.message}`);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected");
+    });
+  });
+  
 app.use(cors(corsOptions))
 app.use(express.json())
 connectDB();
@@ -26,4 +51,4 @@ app.use("/api/streams", streamRoutes);
 app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT||5000
-app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`))
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
